38ac8aee71059350997d564098220cf9960d0a0b
june65 <june0100726@gmail.com>
2024-07-14 16:30:26 +0900

    Fix GraphTERN_visual

--- trajectory_prediction_idea/GraphTERN_visual.py
+++ trajectory_prediction_idea/GraphTERN_visual.py
@@ -59,8 +59,7 @@ class Dataload(Dataset):
         self.seq_order = []
         self.pedestrian_ids = []
 
-        all_files = sorted(Path(self.paths).iterdir())
-        all_files = [file for file in all_files if file.is_file()]
+        all_files = self.paths
 
         num_peds_in_seq = []
         seq_list = []
@@ -68,59 +67,58 @@ class Dataload(Dataset):
         loss_mask_list = []
         non_linear_ped = []
 
-        for files_path in all_files:
-            file_data = Fileload(files_path)
-            times = np.unique(file_data[:, 0]).tolist()
-            time_data = []
-            for time in times:
-                time_data.append(file_data[time == file_data[:, 0], :])
-            num_time = len(times) - self.seq_len + 1
-            
-            for i in range(0, num_time * self.skip + 1, skip):
-                scene_data = np.concatenate(time_data[i:i+self.seq_len],axis=0)
-                #scene_data (dim=2) i~i+seq_len 전체 데이터
-                people_data = np.unique(scene_data[:, 1])
-                self.max_people = max(self.max_people, len(people_data))
+        file_data = Fileload(all_files)
+        times = np.unique(file_data[:, 0]).tolist()
+        time_data = []
+        for time in times:
+            time_data.append(file_data[time == file_data[:, 0], :])
+        num_time = len(times) - self.seq_len + 1
+        
+        for i in range(0, num_time * self.skip + 1, skip):
+            scene_data = np.concatenate(time_data[i:i+self.seq_len],axis=0)
+            #scene_data (dim=2) i~i+seq_len 전체 데이터
+            people_data = np.unique(scene_data[:, 1])
+            self.max_people = max(self.max_people, len(people_data))
 
-                scene = np.zeros((len(people_data), 2, self.seq_len))
-                scene_rel = np.zeros((len(people_data), 2, self.seq_len))
-                scene_loss_mask = np.zeros((len(people_data), self.seq_len))
+            scene = np.zeros((len(people_data), 2, self.seq_len))
+            scene_rel = np.zeros((len(people_data), 2, self.seq_len))
+            scene_loss_mask = np.zeros((len(people_data), self.seq_len))
+            
+            _non_linear_ped = []
+            N_index = 0
+            current_ped_ids = []
+            # N_index = num_peds_considered
+            for _ , people_id in enumerate(people_data):
+                scene_people_xy = scene_data[scene_data[:, 1] == people_id, :]
+                #scene_data[:, 1] = each scene_people id
+                #scene_people = 특정 인물이 들어가 있는 scene_data
+                scene_people_xy = np.around(scene_people_xy, decimals=4)
+                time_front = times.index(scene_people_xy[0, 0]) - i
+                time_end =  times.index(scene_people_xy[-1, 0]) - i + 1
+                if time_end - time_front != self.seq_len:
+                    continue
+                scene_people_xy = np.transpose(scene_people_xy[:, 2:])
+            
+                rel_scene_people = np.zeros(scene_people_xy.shape)
+                rel_scene_people[:, 1:] = scene_people_xy[:, 1:] - scene_people_xy[:, :-1]
+                #rel_scene_people[:, 1:] = 특정 인물의 위치변환 (속도)
+                scene[N_index, :, time_front:time_end] = scene_people_xy
+                scene_rel[N_index, :, time_front:time_end] = rel_scene_people
+                _non_linear_ped.append(poly_fit(scene_people_xy, pred_len, threshold))
                 
-                _non_linear_ped = []
-                N_index = 0
-                current_ped_ids = []
-                # N_index = num_peds_considered
-                for _ , people_id in enumerate(people_data):
-                    scene_people_xy = scene_data[scene_data[:, 1] == people_id, :]
-                    #scene_data[:, 1] = each scene_people id
-                    #scene_people = 특정 인물이 들어가 있는 scene_data
-                    scene_people_xy = np.around(scene_people_xy, decimals=4)
-                    time_front = times.index(scene_people_xy[0, 0]) - i
-                    time_end =  times.index(scene_people_xy[-1, 0]) - i + 1
-                    if time_end - time_front != self.seq_len:
-                        continue
-                    scene_people_xy = np.transpose(scene_people_xy[:, 2:])
-                
-                    rel_scene_people = np.zeros(scene_people_xy.shape)
-                    rel_scene_people[:, 1:] = scene_people_xy[:, 1:] - scene_people_xy[:, :-1]
-                    #rel_scene_people[:, 1:] = 특정 인물의 위치변환 (속도)
-                    scene[N_index, :, time_front:time_end] = scene_people_xy
-                    scene_rel[N_index, :, time_front:time_end] = rel_scene_people
-                    _non_linear_ped.append(poly_fit(scene_people_xy, pred_len, threshold))
-                    
-                    scene_loss_mask[N_index, time_front:time_end] = 1
-                    current_ped_ids.append(people_id) 
-                    N_index += 1
-                
-                if N_index > min_ped:
-                    non_linear_ped += _non_linear_ped
-                    num_peds_in_seq.append(N_index)
-                    loss_mask_list.append(scene_loss_mask[:N_index])
-                    seq_list.append(scene[:N_index])
-                    seq_list_rel.append(scene_rel[:N_index])
-                    self.seqs_used.append(times[i])
-                    self.seq_order.append(i // self.skip)
-                    self.pedestrian_ids.append(current_ped_ids)
+                scene_loss_mask[N_index, time_front:time_end] = 1
+                current_ped_ids.append(people_id) 
+                N_index += 1
+            
+            if N_index > min_ped:
+                non_linear_ped += _non_linear_ped
+                num_peds_in_seq.append(N_index)
+                loss_mask_list.append(scene_loss_mask[:N_index])
+                seq_list.append(scene[:N_index])
+                seq_list_rel.append(scene_rel[:N_index])
+                self.seqs_used.append(times[i])
+                self.seq_order.append(i // self.skip)
+                self.pedestrian_ids.append(current_ped_ids)
         
         self.num_seq = len(seq_list)
         #print(seq_list)
@@ -174,6 +172,16 @@ parser.add_argument('--tag', default='tag', help='Personal tag for the model')
 parser.add_argument('--n_samples', type=int, default=20, help='Number of samples')
 test_args = parser.parse_args()
 
+import matplotlib.colors as mcolors
+def darken_color(color, amount=0.3):
+    try:
+        c = mcolors.cnames[color]
+    except:
+        c = color
+    c = mcolors.to_rgb(c)
+    return [(1 - amount) * x for x in c]
+
+
 def save_gif(_path, predictions, ground_truths, seqs_used, pedestrian_used):
 
     data = Fileload(_path, seqs_used)
@@ -193,10 +201,10 @@ def save_gif(_path, predictions, ground_truths, seqs_used, pedestrian_used):
 
     fig, ax = plt.subplots()
     scatters = {id_: ax.scatter([], [], color=id_color_map[id_]) for id_ in ids}
-    pred_lines = {id_: ax.plot([], [], linestyle='--', color=id_color_map[id_])[0] for id_ in ids}
-    gt_lines = {id_: ax.plot([], [], linestyle='-', color=id_color_map[id_])[0] for id_ in ids}
+    pred_lines = {id_: ax.plot([], [], linestyle='dotted', color=id_color_map[id_])[0] for id_ in ids}
+    gt_lines = {id_: ax.plot([], [], linestyle='-', color=darken_color(id_color_map[id_]))[0] for id_ in ids}
 
-    ax.set_xlim(np.min(data[:, 2]), np.max(data[:, 2]))
+    ax.set_xlim(np.min(data[:, 2]), np.max(data[:, 2])) 
     ax.set_ylim(np.min(data[:, 3]), np.max(data[:, 3]))
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -219,15 +227,23 @@ def save_gif(_path, predictions, ground_truths, seqs_used, pedestrian_used):
                 scatters[id_].set_offsets(offsets)
                 alphas = np.linspace(0, 0.3, len(history[id_]) - 1).tolist() + [0.9]
                 scatters[id_].set_alpha(alphas) 
-                pred_in = predictions[frame][:,ped_id_seq_num[id_][frame]]
-                gt_in = ground_truths[frame][:,ped_id_seq_num[id_][frame]]
-                pred_history[id_].append(pred_in)
-                gt_history[id_].append(gt_in)
-                pred_offsets = np.concatenate(pred_history[id_], axis=0)
-                gt_offsets = np.concatenate(gt_history[id_], axis=0)
-                pred_lines[id_].set_data(pred_offsets[:, 0], pred_offsets[:, 1])
-                gt_lines[id_].set_data(gt_offsets[:, 0], gt_offsets[:, 1])
-                
+                if frame > 8:
+                    if id_ in pedestrian_used[frame - 8] :
+                        pred_in = predictions[frame - 8][:,ped_id_seq_num[id_][frame - 8]]
+                        gt_in = ground_truths[frame - 8][:,ped_id_seq_num[id_][frame - 8]]
+                        pred_history[id_].append(pred_in)
+                        gt_history[id_].append(gt_in)
+                        pred_offsets = np.concatenate(pred_history[id_], axis=0)
+                        gt_offsets = np.concatenate(gt_history[id_], axis=0)
+                        pred_lines[id_].set_data(pred_offsets[:, 0], pred_offsets[:, 1])
+                        gt_lines[id_].set_data(gt_offsets[:, 0], gt_offsets[:, 1])
+
+                    if len(pred_history[id_]) > 0:
+                        pred_history[id_].pop(0)
+                        gt_history[id_].pop(0)
+                    pred_lines[id_].set_alpha(0.5)
+                    gt_lines[id_].set_alpha(0.5)
+
         for id_remove in (np.setdiff1d(ids, pedestrian_used[frame])):
             scatters[id_remove].set_offsets([[-100, -100]])
             pred_lines[id_remove].set_data(-100 * np.ones(12), -100 * np.ones(12))
@@ -241,50 +257,45 @@ def save_gif(_path, predictions, ground_truths, seqs_used, pedestrian_used):
     ani.save(gif_path, writer=animation.FFMpegWriter(bitrate=10000))
     #plt.show()
     
-checkpoint_dir = './checkpoint/' + test_args.tag + '/'
-
-args_path = checkpoint_dir + '/args.pkl'
-with open(args_path, 'rb') as f:
-    args = pickle.load(f)
-
-dataset_path = './datasets/' + args.dataset + '/'
-model_path = checkpoint_dir + args.dataset + '_best.pth'
-
-test_dataset = Dataload(dataset_path + 'test/', obs_len=args.obs_seq_len, pred_len=args.pred_seq_len, skip=1)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
-
 def main():
-    
-    model = graph_tern(n_epgcn=args.n_epgcn, n_epcnn=args.n_epcnn, n_trgcn=args.n_trgcn, n_trcnn=args.n_trcnn,
-                    seq_len=args.obs_seq_len, pred_seq_len=args.pred_seq_len, n_ways=args.n_ways, n_smpl=args.n_smpl)
-    model = model.cuda()
-    model.load_state_dict(torch.load(model_path), strict=False)
 
-    all_predictions = []
-    all_ground_truths = []
-    seq_order = test_dataset.seq_order 
-    pedestrian_ids = test_dataset.pedestrian_ids
-    for batch_idx, batch in enumerate(test_loader):
-        
-        S_obs, S_trgt = [tensor.cuda() for tensor in batch[-2:]]
-        
-        V_init, V_pred, V_refi, valid_mask = model(S_obs, pruning=4, clustering=True)
-        V_refi_new =torch.mean(V_refi, dim=0)
-        all_predictions.append(V_refi_new.cpu().detach().numpy())
-        all_ground_truths.append(S_trgt[:, 1].squeeze(dim=0).cpu().detach().numpy())
-
-    sorted_predictions = [x for _, x in sorted(zip(seq_order, all_predictions))]
-    sorted_ground_truths = [x for _, x in sorted(zip(seq_order, all_ground_truths))]
-
-    save_gif('./datasets/zara1/test/crowds_zara01.txt', sorted_predictions, sorted_ground_truths, test_dataset.seqs_used, test_dataset.pedestrian_ids)
-    
-    '''
     for root, dirs, files in os.walk('./datasets/'):
         #print(files)
         for filename in files:
             if filename.endswith('.txt'):
                 file_path = os.path.join(root, filename)
-                save_gif(file_path)
-    '''
+                dataset_ = root.split('/')[2].split("\\")[0]
+                dataset_path = './datasets/' + dataset_ + '/'
+                checkpoint_dir = './checkpoint/graph-tern_'+ dataset_ +'_experiment/'
+                args_path = checkpoint_dir + '/args.pkl'
+                with open(args_path, 'rb') as f:
+                    args = pickle.load(f)
+                model_path = checkpoint_dir + dataset_ + '_best.pth'
+
+                test_dataset = Dataload(file_path, obs_len=args.obs_seq_len, pred_len=args.pred_seq_len, skip=1)
+                test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
+
+
+                model = graph_tern(n_epgcn=args.n_epgcn, n_epcnn=args.n_epcnn, n_trgcn=args.n_trgcn, n_trcnn=args.n_trcnn,
+                                seq_len=args.obs_seq_len, pred_seq_len=args.pred_seq_len, n_ways=args.n_ways, n_smpl=args.n_smpl)
+                model = model.cuda()
+                model.load_state_dict(torch.load(model_path), strict=False)
+
+                all_predictions = []
+                all_ground_truths = []
+                seq_order = test_dataset.seq_order 
+
+                for batch in test_loader:
+                    S_obs, S_trgt = [tensor.cuda() for tensor in batch[-2:]]
+                    _, _, V_refi, _ = model(S_obs, pruning=4, clustering=True)
+                    V_refi_new =torch.mean(V_refi, dim=0)
+                    all_predictions.append(V_refi_new.cpu().detach().numpy())
+                    all_ground_truths.append(S_trgt[:, 0].squeeze(dim=0).cpu().detach().numpy())
+
+                sorted_predictions = [x for _, x in sorted(zip(seq_order, all_predictions))]
+                sorted_ground_truths = [x for _, x in sorted(zip(seq_order, all_ground_truths))]
+
+                save_gif(file_path, sorted_predictions, sorted_ground_truths, test_dataset.seqs_used, test_dataset.pedestrian_ids)
+                
 if __name__ == "__main__":
     main()
